3004d58d2ca6b70dab1c3168d700e75e3fb3c301
Nucl3arSn3k <shredderofwheat@gmail.com>
2024-02-27 17:01:46 -0500

    Update skeleton_main.c

--- skeleton/skeleton_main.c
+++ skeleton/skeleton_main.c
@@ -1,19 +1,33 @@
- #include <linux/lsm_hooks.h>
+#include <linux/lsm_hooks.h>
+#include <linux/security.h>
+ 
 
-//log stuff
- static int skeleton_bprm_check_security(struct linux_binprm *bprm)
- {
-     printk(KERN_INFO "skeleton LSM check of %s\n", bprm->filename);
-     return 0;
- }
-//Check exec
- static struct security_hook_list skeleton_hooks[] = {
-     LSM_HOOK_INIT(bprm_check_security, skeleton_bprm_check_security),
- };
+static int skel_check (struct linux_binprm *bprm)
+{
+    printk(KERN_INFO "SKELETON LSM check of\n" );
+    return 0;
+}
 
-//Setup
- static void __init skeleton_init(void)
- {
-     security_add_hooks(skeleton_hooks, ARRAY_SIZE(skeleton_hooks), "skeleton");
-     printk(KERN_INFO "skeleton LSM initialized\n");
- }
+
+static struct security_hook_list skeleton_hooks[] = {
+    LSM_HOOK_INIT(bprm_check_security, skel_check),
+};
+
+
+
+
+static int __init sk_init(void)
+{
+    security_add_hooks(skeleton_hooks, ARRAY_SIZE(skeleton_hooks), "Skeleton");
+    printk(KERN_INFO "Skeleton LSM loaded \n");
+    return 0;
+}
+
+
+/*
+ * Ensure the initialization code is called.
+ */
+DEFINE_LSM(can_exec_init) = {
+        .init = sk_init,
+        .name = "skeleton",
+};
