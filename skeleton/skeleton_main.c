0c7df58b89d942879c469cca1c8e1e6c3e1bad88
Nucl3arSn3k <shredderofwheat@gmail.com>
2024-10-20 01:42:39 +0000

    Update skeleton_main.c
    Got mem alloc to work in kernelspace and am attaching stuff to running tasks

--- skeleton/skeleton_main.c
+++ skeleton/skeleton_main.c
@@ -1,104 +1,65 @@
-// lsm.c //this version throws RIP: 0010:skeleton_cred_prepare
 #include <linux/lsm_hooks.h>
 #include <linux/security.h>
 #include <linux/binfmts.h>
 #include <linux/sched.h>
-#include <linux/cred.h>
-#include "include/skeleton.h"
-static int skeleton_bprm_check_security(struct linux_binprm *bprm)
+#include <linux/slab.h>
+#include "skeleton.h"
+//Basic check of files being accessed as a test function
+static int skel_check (struct linux_binprm *bprm)
 {
-    struct skeleton_info *info = current->security;
-    if (info)
-    {
-        printk(KERN_INFO "SKELETON LSM: Process %s (PID: %d) has SKELETON ID %d\n", current->comm, current->pid, info->skeleton_id);
-    }
-    else
-    {
-        printk(KERN_ERR "SKELETON LSM: current->security is null, allocating new skeleton_info (PID: %d)\n",current->pid);
-#if 0
-       info = kzalloc(sizeof(struct skeleton_info), GFP_KERNEL);
-
-        if (!info)
-        {
-            printk(KERN_ERR "SKELETON LSM: Failed to allocate skeleton_info for current process\n");
-            return -ENOMEM;
-        }
-        info->skeleton_id = SKELETON_INVALID;
-        current->security = info;
-        printk(KERN_INFO "SKELETON LSM: Allocated new skeleton_info for current process\n");
-#endif
-    }
+    printk(KERN_INFO "SKELETON LSM check of %s\n",bprm->filename);
     return 0;
 }
 
-static int skeleton_cred_alloc_blank(struct cred *cred, gfp_t gfp)
-{
-    struct skeleton_info *info = kzalloc(sizeof(struct skeleton_info), gfp);
-    if (!info)
-    {
-        printk(KERN_ERR "SKELETON LSM: Failed to allocate skeleton_info during cred_alloc_blank for process (PID: %d)\n",current->pid);
-        return -ENOMEM;
-    }
-    info->skeleton_id = SKELETON_INVALID;
-    cred->security = info;
-    printk(KERN_INFO "SKELETON LSM: Allocated skeleton_info during cred_alloc_blank for process (PID: %d)\n",current->pid);
-    return 0;
+
+
+//Testing kernel memory allocation. Using kzalloc
+int skel_task_alloc(struct task_struct *task,unsigned long clone_flags){
+	printk(KERN_INFO "allocing for %s\n",task->comm);
+	struct skeleton_info *tsec;
+	tsec = kzalloc(sizeof(struct skeleton_info), GFP_KERNEL);
+	if (!tsec){
+		printk(KERN_INFO "alloc failed");
+		return -ENOMEM;
+	}
+
+	tsec->skeleton_id = 22;
+	task->security = tsec;
+	printk(KERN_INFO "alloc functional for task with PID:%d and name:%s\n",task->pid,task->comm);
+	return 0;
 }
 
-static void skeleton_cred_free(struct cred *cred)
-{
-    struct skeleton_info *info = cred->security;
-    if (info)
-    {
-        printk(KERN_INFO "SKELETON LSM: Freeing skeleton_info during cred_free for process (PID: %d)\n",current->pid);
-        kfree(info);
-        cred->security = NULL;
-    }
-    else
-    {
-        printk(KERN_ERR "SKELETON LSM: cred->security is null during cred_free for process (PID: %d)\n",current->pid);
-    }
+
+void skel_task_free(struct task_struct *task){
+	struct skeleton_info *tsec = task->security;
+	if (tsec){
+		printk(KERN_INFO "freeing for %s\n",task->comm);
+		kfree(tsec);
+		task->security = NULL;
+	}
+	
+	
 }
 
-static int skeleton_cred_prepare(struct cred *new, const struct cred *old, gfp_t gfp) //When security field null,this called
-{
-    struct skeleton_info *old_info = old->security;
-    struct skeleton_info *new_info = new->security;
-
-    if (!new_info) {
-        new_info = kzalloc(sizeof(struct skeleton_info), gfp);
-        if (!new_info)
-        {
-            printk(KERN_ERR "SKELETON LSM: Failed to allocate skeleton_info during cred_prepare (PID: %d)\n",current->pid);
-            return -ENOMEM;
-        }
-        new->security = new_info;
-        printk(KERN_INFO "SKELETON LSM: Allocated new skeleton_info during cred_prepare (PID: %d)\n",current->pid);
-    }
-
-    if (old_info) {
-        new_info->skeleton_id = old_info->skeleton_id;
-        printk(KERN_INFO "SKELETON LSM: Copied skeleton_id from old to new during cred_prepare (PID: %d)\n",current->pid);
-    } else {
-        new_info->skeleton_id = SKELETON_INVALID;
-        printk(KERN_ERR "SKELETON LSM: old_info is null during cred_prepare (PID: %d)\n",current->pid);
-    }
-
-    return 0;
-}
 static struct security_hook_list skeleton_hooks[] = {
-    LSM_HOOK_INIT(bprm_check_security, skeleton_bprm_check_security),
-    LSM_HOOK_INIT(cred_alloc_blank, skeleton_cred_alloc_blank),
-    LSM_HOOK_INIT(cred_free, skeleton_cred_free),
-    LSM_HOOK_INIT(cred_prepare, skeleton_cred_prepare),
+    //LSM_HOOK_INIT(bprm_check_security, skel_task_alloc),
+    LSM_HOOK_INIT(task_alloc,skel_task_alloc),
+    LSM_HOOK_INIT(task_free,skel_task_free),
 };
-static int __init skeleton_init(void)
+
+
+static int __init sk_init(void)
 {
-    security_add_hooks(skeleton_hooks, ARRAY_SIZE(skeleton_hooks), "skeleton");
-    printk(KERN_INFO "SKELETON LSM loaded\n");
+    security_add_hooks(skeleton_hooks, ARRAY_SIZE(skeleton_hooks), "Skeleton");
+    printk(KERN_INFO "Skeleton LSM loaded \n");
     return 0;
 }
-DEFINE_LSM(skeleton_lsm) = {
-    .init = skeleton_init,
-    .name = "skeleton",
+
+
+/*
+ * Ensure the initialization code is called.
+ */
+DEFINE_LSM(can_exec_init) = {
+        .init = sk_init,
+        .name = "skeleton",
 };
