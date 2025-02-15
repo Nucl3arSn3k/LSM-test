139ac9e4c6bebb93554e22149e3b37d9602867fe
Nucl3arSn3k <shredderofwheat@gmail.com>
2025-02-15 01:38:27 +0000

    Update file.h
    adding stuff for xattrs

--- skeleton/file.h
+++ skeleton/file.h
@@ -9,21 +9,27 @@
 #include <linux/fs.h>
 //This builds fine
 struct fl_min{
-	atomic_t ref_count;
-	const char *appid;
-	unsigned int perms;
+	atomic_t ref_count;//potentially start without this
+	int appid; //Simplified from string,minimize complexity
+	unsigned int perms; //Perm "field"
+};
+
+struct x_value{ //Just a custom struct for XATTRS
+      int appid; //Stores the APPID or something
+      unsigned int perms; //Stores perms
 };
 
 
+
 struct fl_nest{
-	spinlock_t lock; //protect label when updateing
+	spinlock_t lock; //protect label when updating
 	struct fl_min __rcu *min; //protected for concurrent reads
 };
 
 //Function prototpying
 
-struct fl_min *create_label_min(const char *appid);
-struct fl_nest *get_fl(struct file *file);
+struct fl_min *create_label_min(int appid);
+struct fl_nest *get_fl(struct inode *node);
 void put_min(struct fl_min *label);
-void free_min(struct fl_min *label);
+void free_min(struct fl_min *label); //Free it
 #endif /* FILE.H */
