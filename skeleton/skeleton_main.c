7217fb5326c6ca2b6cfd3f5d566fc0b0f4d4d05c
Nucl3arSn3k <shredderofwheat@gmail.com>
2025-03-05 18:09:58 +0000

    Update skeleton_main.c

--- skeleton/skeleton_main.c
+++ skeleton/skeleton_main.c
@@ -9,14 +9,15 @@
 #include <linux/spinlock.h>
 #include <linux/atomic.h>
 #include <linux/init.h>
-#include "skeleton.h" //Not actually used, just remove later
+#include "skeleton.h" //Not sure if this is actually being used,but I suspect not
 #include "file.h"
 #include "xattrhandle.h"
 //Problem of making attributes PERSIST.
 //not entirely clear,additional security info on inodes,and HOW does that info get to and from disk?
-
+//Creates a blob so the nested structure plays nice with other security modules
 struct lsm_blob_sizes skeleton_blob_sizes __ro_after_init = { //blob sizes set
   .lbs_inode = sizeof(struct fl_nest),
+  .lbs_task = sizeof(struct process_attatched),
 
 };
 
@@ -63,7 +64,7 @@ void put_min(struct fl_min *label) { //decrement and check label,free if 0
     }
 }
 
-// New function to get and increment atromic reference count
+// New function to get and increment atomic reference count
 struct fl_min *get_min(struct fl_min *label) {
     if (label) {
         atomic_inc(&label->ref_count);
@@ -78,7 +79,7 @@ struct fl_nest *set_fl(struct inode *node){ //Create the holding struct
 	        printk(KERN_INFO "Skeleton LSM: Skipping label during early boot\n");
 	        return NULL;
     	}
-	int app_id = 0xDEADBEEF;
+	int app_id = 0xDEADBEEF; //Sets to this instead of real APPID
 	struct fl_nest *contained = node->i_security; //fl = file label
 	
 	
@@ -122,13 +123,13 @@ struct fl_nest *get_fl(struct inode *node) { //Get a label if needed
 void skl_inode_free(struct inode *file) { //Free file security field
     struct fl_nest *wrapper = file->i_security;
     if (wrapper) {
-	printk(KERN_DEBUG "Skeleton LSMv4: Freeing security for inode %lu\n", file->i_ino);
+	printk(KERN_DEBUG "Skeleton LSMv6: Freeing security for inode %lu\n", file->i_ino); //Tweak v num with everybuild
         
         free_nest(wrapper);
         //file->i_security = NULL;
     }
 }
-
+//Allocate the INODE security field
 static int skl_alloc_inode(struct inode *node) { //Extended attribute calls to actually put this on disk
     struct fl_nest *nest;
     
@@ -142,18 +143,61 @@ static int skl_alloc_inode(struct inode *node) { //Extended attribute calls to a
     return 0;
 }
 
+
+static int skl_alloc_procsec(struct task_struct *task, unsigned long clone_flags){ //Allocate task security struct
+  if (!task){
+    return -EINVAL;
+  }
+  struct process_attatched *contained; 
+  contained = task->security;
+  contained->appid = 1;
+  contained->perms = 42;
+  
+  if (IS_ERR(contained)){
+    return PTR_ERR(contained);
+  }
+  printk(KERN_INFO "Security structure allocated for task %s\n",task->comm); //Should be able to directly grab from comm field
+  return 0;
+
+}
+
+static void skl_procsec_free(struct task_struct *task){
+  struct process_attatched *label = task->security;
+  printk("Freeing appid security structure");
+
+
+}
+
+
+
 //Let's document!
 //qstr is a custom struct for a string that has the hash?
 //Xattrs are used in some sort of odd callback that's not the direct implementation,but looks like some type of VFS abstraction over EXT4,NTFS,what have you
-//dir and node fields on inode struct (obviously) 
+//need to figure out what dir does
+//explain input and return types.
 static int skl_init_security(struct inode *node, struct inode *dir, const struct qstr *qstr,const char **name,void **value,size_t *len){
   *name = "security.skl"; //Name set
-  struct x_value *new = create_xattr_struct(22); //Value set
+  struct task_struct *mytask = current; //use current macro?
+  struct process_attatched *locstruct; //Should probably nullcheck this
+  int hard = 0;
+  if(mytask && mytask->security){
+    locstruct = mytask->security;
+    hard = locstruct->appid;
+    printk(KERN_INFO "Got appid %d from task %s\n",hard,mytask->comm);
+  }
+  else{
+    hard = 22;
+    printk(KERN_INFO "Using fallback appid %d from task %s\n",hard,mytask->comm);
+  }
+  struct x_value *new = create_xattr_struct(hard); //Value set from hard
+  
   if (IS_ERR(new)){
     return PTR_ERR(new);
   }
-  *value = new;
+  *value = new; //passed back as void. Implict cast?
   *len = sizeof(struct x_value);
+ //Does it expect pointer to data structure and size?
+  printk("Setting XATTR for task %s on inode %lu,id value %d\n",mytask->comm,node->i_ino,new->appid);
 
   return 0;
 } 
@@ -172,6 +216,9 @@ static struct security_hook_list skeleton_hooks[] = {
     LSM_HOOK_INIT(inode_free_security, skl_inode_free),
     LSM_HOOK_INIT(inode_alloc_security, skl_alloc_inode),
     LSM_HOOK_INIT(inode_init_security, skl_init_security),
+    LSM_HOOK_INIT(task_alloc,skl_alloc_procsec),
+    LSM_HOOK_INIT(task_free,skl_procsec_free),
+    
     //LSM_HOOK_INIT(file_alloc_security, skel_file_alloc_security),
     //LSM_HOOK_INIT(file_free_security, skel_file_free_security),
 };
