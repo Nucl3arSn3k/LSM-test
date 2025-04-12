8cdc2b7b1bd3c760b512497d8c5d4bb78c1ae407
Nucl3arSn3k <shredderofwheat@gmail.com>
2025-04-12 19:15:49 +0000

    Update skeleton.h

--- skeleton/skeleton.h
+++ skeleton/skeleton.h
@@ -2,10 +2,15 @@
 #define _SKELETON_H_
 
 #define SKELETON_INVALID -1
+#include "file.h"
+extern struct lsm_blob_sizes skeleton_blob_sizes;
 
+static inline struct fl_nest *skeleton_inode(const struct inode *inode){
+  return inode->i_security + skeleton_blob_sizes.lbs_inode;
+}
 
-struct skeleton_info{
-	int skeleton_id;
-};
+static inline struct process_attatched *skeleton_task(struct task_struct *task){
+  return task->security + skeleton_blob_sizes.lbs_task;
+}
 
 #endif /* _SKELETON_H_ */
