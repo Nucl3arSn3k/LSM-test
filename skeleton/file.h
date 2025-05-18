ebd24414f812e96a27c7a079cd62dae89bbfaa4f
Nucl3arSn3k <shredderofwheat@gmail.com>
2025-05-18 16:22:09 -0400

    Update file.h
    taking out lock

--- skeleton/file.h
+++ skeleton/file.h
@@ -10,7 +10,6 @@
 #include <linux/fs.h>
 //This builds fine
 struct fl_min{
-	atomic_t ref_count;//potentially start without this
 	int appid; //Simplified from string,minimize complexity
 	unsigned int perms; //Perm "field"
 };
@@ -27,15 +26,12 @@ struct process_attatched{ //Security field for process filler
 };
 
 
-struct fl_nest{
-	spinlock_t lock; //protect label when updating
-	struct fl_min __rcu *min; //protected for concurrent reads
-};
+
 
 //Function prototpying
 
 struct fl_min *create_label_min(int appid);
-struct fl_nest *get_fl(struct inode *node);
+
 void put_min(struct fl_min *label);
 void free_min(struct fl_min *label); //Free it
 #endif /* FILE.H */
